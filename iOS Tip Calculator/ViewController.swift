5cfde8033f67924106a5d393f52e8df3aead16f8
Anoop Khera <akhera44@gmail.com>
2020-12-22 15:40:42 -0500

    Initial Commit

---
+++ iOS Tip Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Tip Calculator
+//
+//  Created by Anoop Khera on 12/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
